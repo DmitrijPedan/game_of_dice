81503833661741eb1e42dc2d341a6d5684114819
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-01-09 10:29:24 +0200

    add random and Gamer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('Game of dice started ...');
+
+const getRandomNumber = (min, max) =>  Math.floor(Math.random()*(max - min + 1) + min);
+
+function Gamer (name, budget = 1000) {
+    this.name = name,
+    this.budget = budget,
+    this.winMessage = function () {console.log(`${this.name}, you win!`)},
+    this.lostMessage = function () {console.log(`${this.name}, you lost!`)}
+};
+
+const newGamer = new Gamer('Dima');
