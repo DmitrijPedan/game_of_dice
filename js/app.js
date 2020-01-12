5efcd632343c3f0203992222fc8050227ba3ba9c
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-01-12 12:55:39 +0200

    fix bet input

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
-
 function GetUserInfo() {
-    this.user = {name: '', budget: 0, maxBudget: 0}
-    this.user.name = prompt('Ваше имя');
-    this.user.budget = 1000; //+prompt('Стартовый бюджет:');
-    this.user.maxBudget = 10000; //+prompt('Играем до:');
-    return this.user;
+    this.name = prompt('Ваше имя:');;
+    this.budget = 1000; //+prompt('Стартовый бюджет:');
+    this.maxBudget = 10000; //+prompt('Играем до:');
+    this.defaultName = () => {this.name = 'Игрок'};
+    (this.name == '') ? this.defaultName() : name;
 }
 
 function RollTheDices () {
-    this.value = [1, 6];
+    this.value = [2, 3];
     this.getRandomValue = (min, max) =>  Math.floor(Math.random()*(max - min + 1) + min);
     this.dice1 = this.getRandomValue(...this.value);
     this.dice2 = this.getRandomValue(...this.value);
@@ -29,9 +28,7 @@ function updateGameInfo() {
 }
 
 function createGameAlert (type, text) {
-    if (document.getElementById("gameAlert")){
-        gameAlert.remove();
-    }        
+    document.getElementById("gameAlert") ? gameAlert.remove() : false;    
     let div = document.createElement('div');
     div.className = `alert alert-${type}`;
     div.role ="alert";
@@ -41,9 +38,7 @@ function createGameAlert (type, text) {
 }
 
 function createDicesAlert (type, text) {
-    if (document.getElementById("diceAlert")){
-        diceAlert.remove();
-    }        
+    clearMessage ();  
     let div = document.createElement('div');
     div.className = `alert alert-${type}`;
     div.role ="alert";
@@ -52,37 +47,57 @@ function createDicesAlert (type, text) {
     diceAlert.innerHTML = text;
 }
 
+function createBetAlert (type, text) {
+    clearMessage ()      
+    let div = document.createElement('div');
+    div.className = `alert alert-${type}`;
+    div.role ="alert";
+    div.id = "betAlert";
+    message.prepend(div);
+    betAlert.innerHTML = text;
+}
+
+function clearMessage () {
+    document.getElementById("gameAlert") ? gameAlert.remove() : false;
+    document.getElementById("diceAlert") ? diceAlert.remove() : false;
+    document.getElementById("betAlert") ? betAlert.remove() : false;
+}
+
 function letGame () {
-    if (gamer.budget > 0 && gamer.budget < gamer.maxBudget) {
-        let bet = +inputBet.value;
-        let number = +inputNumber.value;
-        let step = new RollTheDices();
-        history.push(step);
-        createDicesAlert('primary', `Выпало <span class="badge badge-primary result">${step.summ}</span> (${step.dice1} и ${step.dice2}).`);
-        // console.log(`Выпало: ${step.summ}. (${step.dice1} и ${step.dice2}).`);
-            switch (true) {
-                case (step.dice1 == step.dice2 && step.summ == number):
-                    createGameAlert('info', `Вы загадали <span class="badge badge-info result">${number}</span>. Вы угадали и кости совпали! ставка х3`);
-                    gamer.budget += (bet*3);
-                break;
-                case (step.summ == number):
-                    createGameAlert('success', `Вы загадали <span class="badge badge-success result">${number}</span> и угадали. ставка х2`);
-                    gamer.budget += (bet*2);
-                break;
-                case (step.summ != number):
-                    createGameAlert('warning', `Вы загадали <span class="badge badge-danger result">${number}</span> и вы не угадали`);
-                    gamer.budget = gamer.budget - bet;
-                break;
-            }
-            updateGameInfo();
-    }
-    switch (true) {
-        case (gamer.budget <= 0):
-            createGameAlert('danger', `<span class="badge badge-danger result">${gamer.name}</span> Вы проиграли. Конец игры`);
-        break;
-        case (gamer.budget >= gamer.maxBudget):
-            createGameAlert('success', `<span class="badge badge-success result">${gamer.name}</span> Вы выиграли <span class="badge badge-success result">${gamer.budget}</span> !`);
-        break;
+   if (+inputBet.value > 0 && +inputBet.value <= gamer.budget) 
+    {
+        if (gamer.budget > 0 && gamer.budget < gamer.maxBudget) {
+            let bet = +inputBet.value;
+            let number = +inputNumber.value;
+            let step = new RollTheDices();
+            history.push(step);
+            createDicesAlert('primary', `Выпало <span class="badge badge-primary result">${step.summ}</span> (${step.dice1} и ${step.dice2}).`);
+                switch (true) {
+                    case (step.dice1 == step.dice2 && step.summ == number):
+                        createGameAlert('info', `Вы загадали <span class="badge badge-info result">${number}</span>. Вы угадали и кости совпали! ставка х3`);
+                        gamer.budget += (bet*3);
+                    break;
+                    case (step.summ == number):
+                        createGameAlert('success', `Вы загадали <span class="badge badge-success result">${number}</span> и угадали. ставка х2`);
+                        gamer.budget += (bet*2);
+                    break;
+                    case (step.summ != number):
+                        createGameAlert('warning', `Вы загадали <span class="badge badge-danger result">${number}</span> и вы не угадали`);
+                        gamer.budget = gamer.budget - bet;
+                    break;
+                }
+                updateGameInfo();
+        }
+        switch (true) {
+            case (gamer.budget <= 0):
+                createGameAlert('danger', `<span class="badge badge-danger result">${gamer.name}</span> Вы проиграли. Конец игры`);
+            break;
+            case (gamer.budget >= gamer.maxBudget):
+                createGameAlert('success', `Поздравляем! <span class="badge badge-success result">${gamer.name}</span> Вы выиграли <span class="badge badge-success result">${gamer.budget}</span> !`);
+            break;
+        }
+    } else {
+        createBetAlert('danger', `Ставка должна быть в пределах бюджета (от 1 до ${gamer.budget})`);
     }
 };
 
